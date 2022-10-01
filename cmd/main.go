8ea20295499b6c7f2292d75b2ef20e0f059059c6
root <nameerrror3301@gmail.com>
2022-10-01 14:26:40 +0200

    I combined everything, and now I have to prescribe the logic of the bot.

---
+++ cmd/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	socks "GoFLParcerXML/lib"
+	"flag"
+	"fmt"
+	"os"
+)
+
+var (
+	tgBotToken string
+)
+
+// Function for getting a token
+func init() {
+	flag.StringVar(&tgBotToken, "tgBotToken", "", "Telegram Bot Token")
+	flag.Parse()
+	if tgBotToken == "" {
+		fmt.Println("-tgBotToken is not defined")
+		os.Exit(1)
+	}
+}
+
+func main() {
+	socks.GetXmlItem("https://hidemy.name/ru/proxy-list/?type=5#list", "https://www.fl.ru/rss/all.xml")
+}
